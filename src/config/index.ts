2ef13dbc8d5a740df5754a930db4075c7897793e
mstsurnalyakter <surnalyms@gmail.com>
2025-10-08 20:39:30 +0600

    feat: update dependencies in package.json, add autoprefixer to PostCSS config, and adjust globals.css imports

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const envConfig = {
-    backendURL:process.env.NEXT_PUBLIC_BACKEND_URL
+    backendURL:process.env.NEXT_PUBLIC_BACKEND_URL || "https://found-x-server-kappa.vercel.app/api/v1"
 }
 
 export default envConfig;
